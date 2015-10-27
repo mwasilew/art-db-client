1e4cbe76cb13fe267d13c7b6d13c06c239949708
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2015-10-27 11:12:26 +0100

    art script for sending manifest

--- art.py
+++ art.py
@@ -7,39 +7,47 @@ URL = "http://10.0.0.100:8000/api"
 
 
 @click.group()
-def cli():
-    pass
+@click.option('--url', default=URL, help="default connection url current: '%s'" % URL)
+@click.pass_context
+def cli(ctx, url):
+    ctx.obj = {'url': url}
 
 
-@click.command()
-@click.argument('xml', type=click.File('rb'))
-def manifest(xml):
-    manifest_content = xml.read()
+@click.command(help="Uploads xml manifests")
+@click.argument('manifest-file', type=click.File('rb'))
+@click.pass_context
+def manifest(ctx, manifest_file):
+    api_url = ctx.obj['url']
+    manifest_content = manifest_file.read()
     hash = hashlib.sha1(manifest_content).hexdigest()
 
-    response = requests.post("%s/manifest/" % URL,
-                             data={"manifest": manifest_content})
+    try:
+        response = requests.post("%s/manifest/" % api_url, timeout=10, data={"manifest": manifest_content})
 
-    if response.status_code == 201:
-        click.echo(click.style("Manifest: '%s' sent." % hash, fg='green'))
+        if response.status_code == 201:
+            click.echo(click.style("Manifest: '%s' sent." % hash, fg='green'))
 
-    elif response.status_code == 400:
-        msg = "".join(["%s: %s" % (name, ", ".join(errors))
+        elif response.status_code == 400:
+            msg = "".join(["%s: %s" % (name, ", ".join(errors))
                        for name, errors in response.json().items()])
+            click.echo(click.style('Upload Error: "%s"' % msg, fg='yellow'))
+        else:
+            click.echo(click.style("Upload Error", fg='red'))
 
-        click.echo(click.style('Upload Error: "%s"' % msg, fg='yellow'))
-    else:
-        click.echo(click.style("Upload Error", fg='red'))
+    except requests.exceptions.ConnectionError as e:
+        click.echo(click.style("Connection Error, server '%s' down?" % api_url, fg='red'))
 
 
-# @click.argument('manifest', type=click.File('rb'))
+
 @click.command()
 def results():
     click.echo("Looking")
 
 
+
 cli.add_command(manifest)
 cli.add_command(results)
+
 
 if __name__ == '__main__':
     cli()
