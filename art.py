6138f8da6b2a889c3a794afd49c9103b64a0dbe6
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2015-10-28 09:48:53 +0100

    formatting

--- art.py
+++ art.py
@@ -22,21 +22,23 @@ def manifest(ctx, manifest_file):
     hash = hashlib.sha1(manifest_content).hexdigest()
 
     try:
-        response = requests.post("%s/manifest/" % api_url, timeout=10, data={"manifest": manifest_content})
+        response = requests.post(
+            "%s/manifest/" % api_url,
+            timeout=10,
+            data={"manifest": manifest_content})
 
         if response.status_code == 201:
             click.echo(click.style("Manifest: '%s' sent." % hash, fg='green'))
 
         elif response.status_code == 400:
             msg = "".join(["%s: %s" % (name, ", ".join(errors))
-                       for name, errors in response.json().items()])
-            click.echo(click.style('Upload Error: "%s"' % msg, fg='yellow'))
+                           for name, errors in response.json().items()])
+            click.echo(click.style("Upload Error: '%s'" % msg, fg='yellow'))
         else:
             click.echo(click.style("Upload Error", fg='red'))
 
-    except requests.exceptions.ConnectionError as e:
+    except requests.exceptions.ConnectionError:
         click.echo(click.style("Connection Error, server '%s' down?" % api_url, fg='red'))
-
 
 
 @click.command()
@@ -44,10 +46,12 @@ def results():
     click.echo("Looking")
 
 
-
 cli.add_command(manifest)
 cli.add_command(results)
 
+
+def main():
+    cli()
 
 if __name__ == '__main__':
     cli()
