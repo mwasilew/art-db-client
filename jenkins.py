9ca5ac8282c5817ce7631723a9d6c437956ab528
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2016-01-04 15:08:52 +0100

    option for token authentication added jenkins script, better error presentation

--- jenkins.py
+++ jenkins.py
@@ -10,26 +10,44 @@ def colon_separted_type(item):
             "should be in form ITEM_NAME:ITEM_VALUE")
     return value
 
+def pretty_errors(errors):
+    return ", ".join(
+        ['"%s" - %s' % (error, ", ".join(messages).lower())
+         for error, messages in errors.items()])
+
 
 def main():
     parser = argparse.ArgumentParser(
-        description='upload data to ART instance')
+        description='Upload data to ART instance')
 
-    parser.add_argument('url', help='ART instance url')
-
-    parser.add_argument('values', metavar='item_name:item_value',
-                        type=colon_separted_type, nargs='+',
-                        help='a list of values to send to ART instance')
+    parser.add_argument('url', help='Instance url')
+    parser.add_argument('token', help='Authentication token')
+    parser.add_argument('values', nargs='+',
+                        metavar='item_name:item_value',
+                        type=colon_separted_type,
+                        help='List of values to send to ART instance')
 
     args = parser.parse_args()
     try:
-        response = requests.post(args.url, data=dict(args.values))
-        response.raise_for_status()
+        headers = {"Authorization": "Token %s" % args.token}
+        data = dict(args.values)
 
-        sys.stdout.write("OK\n")
+        response = requests.post(args.url, headers=headers, data=data)
+
+        if response.status_code == requests.codes.ok:
+            sys.stdout.write("OK\n")
+            sys.exit()
+
+        if response.status_code == requests.codes.bad:
+            errors = pretty_errors(response.json())
+            sys.stderr.write("Validation Errors: %s\n" % errors)
+            sys.exit(1)
+
+        response.raise_for_status()
 
     except requests.exceptions.RequestException as e:
         sys.stderr.write("Error: %s\n" % str(e))
+        sys.exit(1)
 
 
 if __name__ == '__main__':
--- tests/test_jenkins.py
+++ tests/test_jenkins.py
@@ -9,19 +9,41 @@ def test_colon_separted_type_1():
     with pytest.raises(argparse.ArgumentTypeError):
         jenkins.colon_separted_type("name:")
 
+
 def test_colon_separted_type_2():
     with pytest.raises(argparse.ArgumentTypeError):
         jenkins.colon_separted_type(":value")
+
 
 def test_colon_separted_type_3():
     with pytest.raises(argparse.ArgumentTypeError):
         jenkins.colon_separted_type(":")
 
+
 def test_colon_separted_type_4():
     with pytest.raises(argparse.ArgumentTypeError):
         jenkins.colon_separted_type("")
 
+
 def test_colon_separted_type_5():
     assert len(jenkins.colon_separted_type("namex:value")) == 2
     assert len(jenkins.colon_separted_type("item_name:item_value")) == 2
+
+
+def test_pretty_errors_1():
+    errors = {"id": ["one error"]}
+    assert '"id" - one error' == jenkins.pretty_errors(errors)
+
+
+def test_pretty_errors_2():
+    errors = {"id": ["one error", "two error"]}
+    assert '"id" - one error, two error' == jenkins.pretty_errors(errors)
+
+
+def test_pretty_errors_3():
+    errors = {"id": ["one error"],
+              "url": ["one error"]}
+
+    assert '"id" - one error' in jenkins.pretty_errors(errors)
+    assert '"url" - one error' in jenkins.pretty_errors(errors)
 
