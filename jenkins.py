f219b89e13ba4ad656492d890d7a27c4eb3a3534
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2016-01-27 14:34:56 +0000

    jenkins.py: fixed print formatting error
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- jenkins.py
+++ jenkins.py
@@ -52,7 +52,7 @@ def _push_object(auth_pw, backend_url, endpoint, params):
 
 def _get_manifest(workspace_path):
     manifest_path = workspace_path + "pinned-manifest.xml"
-    print "Searching for: %" % manifest_path
+    print "Searching for: %s" % manifest_path
     if os.path.exists(manifest_path):
         with open(manifest_path, "r") as manifest_file:
             return manifest_file.read()
